76d596cbfc9214bb25d3da792c4c810d08ef1899
Hammerhead06 <134006602+Hammerhead06@users.noreply.github.com>
2023-11-02 12:54:50 +0000

    comparisons done

---
+++ 01_Basics/04_comparisons.js
@@ -0,0 +1,13 @@
+console.log(2>1);
+console.log(2!=1);
+
+console.log("2">1);
+
+console.log(null>1);
+console.log(null==0);
+
+// aisi equality check nhi krna chahiye hame
+
+// === ->strict check =>value & datatype dono ko check krta hai
+
+console.log("2"===2);
