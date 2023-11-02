90a7ec50b099b8833a82a098bd8c86528b49cbb6
Hammerhead06 <134006602+Hammerhead06@users.noreply.github.com>
2023-11-02 12:45:39 +0000

    operations done

--- 01_Basics/03_conversion_operation.js
+++ 01_Basics/03_conversion_operation.js
@@ -9,3 +9,15 @@ console.log(num);
 // "33"=>33
 // "33abs"=>NaN
 // true =>1
+
+// ************************************Operations*******************************************
+
+let val=3;
+val=-val
+console.log(val);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);  // =>122 dega 
+console.log(1+2+"2");  //=>32 dega
+
