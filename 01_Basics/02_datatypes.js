39a25164cde928663b3926c845a4ffd3d039c70c
Hammerhead06 <134006602+Hammerhead06@users.noreply.github.com>
2023-11-02 13:24:33 +0000

    return type of datatypes

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -5,3 +5,20 @@ let islogin=false
 
 // null ->standalone value
 // undefined -> value assign nhi hui hai
+
+
+// ye yaad kr lo
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
