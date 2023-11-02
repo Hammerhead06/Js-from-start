63b5cb9ec6a89af1ba2b25724511f382eee85696
Hammerhead06 <134006602+Hammerhead06@users.noreply.github.com>
2023-11-02 11:39:29 +0000

    datatypes done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+let x="hello world";
+var y="2311232";
+const z="roshan kumar";
+
+// z="ankesh kumar" =>you can't change const variable
+/*
+prefer not to use var because it has problems in scope - such as for loop
+*/
+
+console.table([x,y,z])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; //treat all the code in newer format
+let name="roshan";
+let age = 20;
+let islogin=false
+
+// null ->standalone value
+// undefined -> value assign nhi hui hai
