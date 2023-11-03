ecc83d7d0d8af6c79d7efefbb3d8e28a7188edc4
Hammerhead06 <134006602+Hammerhead06@users.noreply.github.com>
2023-11-03 02:37:00 +0000

    basic of array done

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,16 @@
+const myArr=[0,13,5,6,7]
+console.log(myArr[1])
+
+//array methods
+myArr.push(32); //myArr.pop()
+myArr.unshift(9) //push front
+myArr.shift() //pop krta hai front se
+console.log(myArr);
+console.log(myArr.indexOf(121)); 
+
+
+// const newArr=myArr.join(); //array ko string me change kr dega
+// console.log(myArr)
+// console.log(newArr)
+
+// differece between slice and splice
